bef4f10dbb1341f8e739265b319f76ce45178528
DESKTOP-153DOJA\Kenneth <arceniokas@gmail.com>
2018-12-08 15:43:44 +0800

    filter by name

--- src/app/hotels/hotels.component.ts
+++ src/app/hotels/hotels.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { HotelService } from './hotel.service';
 import { Hotel } from './hotel.model';
-import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FilterHeader } from '../shared/filter-header/filter-header.model';
+import { ArrayType } from '@angular/compiler/src/output/output_ast';
 
 
 
@@ -10,25 +12,38 @@ import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.component.html',
-  styleUrls:['./hotels.component.css']
+  styleUrls: ['./hotels.component.css']
 })
 export class HotelsComponent implements OnInit {
 
   private hotels: Array<Hotel>;
+  private filteredHotels: Array<Hotel>;
 
   constructor(private hotelService: HotelService,
     ratingConfig: NgbRatingConfig) {
-      ratingConfig.max=10;
-      ratingConfig.readonly=true;
+    ratingConfig.max = 10;
+    ratingConfig.readonly = true;
 
-     }
+  }
 
   ngOnInit() {
 
-    this.hotelService.getHotels().subscribe(serviceHotels=>{
-      console.log(serviceHotels);
-      this.hotels=serviceHotels;
+    this.hotelService.getHotels().subscribe(serviceHotels => {
+      this.hotels = serviceHotels;
+      this.filteredHotels = serviceHotels;
     });
+  }
+  
+  filterChange(filterHeader: FilterHeader) {
+    if (filterHeader.name != null) {
+      this.filterByName(filterHeader.name);
+    }
+  }
+
+  private filterByName(hotelName: string){
+    this.filteredHotels = this.hotels.filter(hotel => 
+      hotel.name.toLowerCase().includes(hotelName.toLowerCase())
+    );
   }
 
 }
---
+++ src/app/shared/filter-header/filter-header.model.ts
@@ -0,0 +1,8 @@
+export class FilterHeader{
+    public name:string;
+    public minReviewRating:number;
+    public maxReviewRating:number;
+    public minPrice: number;
+    public maxPrice: number;
+    public starRating: Array<number>;
+}
